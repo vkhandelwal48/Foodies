4ac0058cccc1f922fa8575972003dbabb23a533a
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-20 00:42:24 +0530

    added meal item and meal grid

--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,7 +1,6 @@
 export default function MealsLayout({ children }) {
     return (
         <div>
-            <h1>Meals</h1>
             {children}
         </div>
     );
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,20 @@
+import MealsGrid from "../components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function MealsPage() {
     return (
-        <h1>Meals</h1>
+        <>
+        <header className={classes.header}>
+            <h1>Delicious Meals, Created {' '}<span className={classes.highlight}> by you</span></h1>
+            <p>Choose your favorite recipe and cook it yourself. It is easy and fun.</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">Share your favorite recipe</Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
+        </main>
+        </>
     );
 }
