1ae823f3ca49302617270850b755daf66e3fb4a5
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-20 20:04:42 +0530

    added not found for meal

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
     const meal = getMeal(params.slug);
+    if (!meal) {
+        notFound();
+    }
     meal.instructions = meal.instructions.replace(/\n/g, '<br>'); // Convert newlines to <br> for HTML rendering
     return (
         <>
