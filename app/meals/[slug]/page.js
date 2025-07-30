775b51c31796ea0b5d839d916f7611da591bc630
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-30 21:53:15 +0530

    added dynamic metadata

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,19 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) { // this function receives same data as the page component
+    const meal = await getMeal(params.slug);
+
+    if(!meal) {
+        notFound();
+    }
+    
+    return {
+        title: meal.title,
+        description: meal.summary,
+    }
+}
+
 export default function MealDetailsPage({ params }) {
     const meal = getMeal(params.slug);
     if (!meal) {
