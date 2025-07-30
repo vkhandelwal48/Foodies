ecf90caa50006a38ba3f810ec483128cbe59e94d
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-30 22:10:45 +0530

    Fixed params await issue

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -4,20 +4,22 @@ import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) { // this function receives same data as the page component
-    const meal = await getMeal(params.slug);
+    const awaitedParams = await params;
+    const meal = await getMeal(awaitedParams.slug);
 
     if(!meal) {
         notFound();
     }
-    
+
     return {
         title: meal.title,
         description: meal.summary,
     }
 }
 
-export default function MealDetailsPage({ params }) {
-    const meal = getMeal(params.slug);
+export default async function MealDetailsPage({ params }) {
+    const awaitedParams = await params;
+    const meal = await getMeal(awaitedParams.slug);
     if (!meal) {
         notFound();
     }
